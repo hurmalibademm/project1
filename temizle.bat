de325da4960612c8f2f2fa255ca50f90159fbf67
hurmalibademm <161161190+hurmalibademm@users.noreply.github.com>
2024-03-02 11:47:54 +0300

    Add files via upload

--- temizle.bat
+++ temizle.bat
@@ -26,34 +26,57 @@ del /s /f /q %WinDir%\Prefetch\*.*
 del /s /f /q %Temp%\*.*
 del /s /f /q %AppData%\Temp\*.*
 del /s /f /q %HomePath%\AppData\LocalLow\Temp\*.*
-
+cls
 rem Delete Used Drivers Files (Not needed because already installed)
 del /s /f /q %SYSTEMDRIVE%\AMD\*.*
 del /s /f /q %SYSTEMDRIVE%\NVIDIA\*.*
 del /s /f /q %SYSTEMDRIVE%\INTEL\*.*
-
+cls
 rem Delete Temporary Folders
 rd /s /q %WinDir%\Temp
 rd /s /q %WinDir%\Prefetch
 rd /s /q %Temp%
 rd /s /q %AppData%\Temp
 rd /s /q %HomePath%\AppData\LocalLow\Temp
-
+clscls
 rem Delete Used Drivers Folders (Not needed because already installed)
 rd /s /q %SYSTEMDRIVE%\AMD
 rd /s /q %SYSTEMDRIVE%\NVIDIA
 rd /s /q %SYSTEMDRIVE%\INTEL
-
+cls
 rem Recreate Empty Temporary Folders
 md %WinDir%\Temp
 md %WinDir%\Prefetch
 md %Temp%
 md %AppData%\Temp
 md %HomePath%\AppData\LocalLow\Temp
+cls
 
-echo.
-echo Windows temizlendi belirli araliklarla ara sira calistirabilirsiniz, cikmak icin bir tusa basin.
-echo.
+rem Belirtilen klasördeki gereksiz dosyaları ve klasörleri silme işlemi
+set "targetFolder=%LocalAppData%"
+
+rem Geçici değişkenler
+set "deletedFolders=0"
+
+rem Alt klasörleri döngüye al
+for /d %%i in ("%targetFolder%\tw-*") do (
+    rem Klasörü sil
+    rem echo Silinen klasör: "%%i"
+    rd /s /q "%%i"
+    set /a deletedFolders+=1
+)
+
+rem Kullanıcıya silinen klasör sayısını bildir
+
+cls
+color 0A
+setlocal enabledelayedexpansion
+set /a width=80
+set /a spaces=(%width%-%deletedFolders%-36)/2
+set "space="
+for /l %%i in (1,1,%spaces%) do set "space=!space! "
+echo !space!Windows temizlendi ve ek olarak %deletedFolders% gereksiz klasor silindi.!space!
+endlocal
 
 pause > NUL
 exit
