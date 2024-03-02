edab9c9aa19d039cfb0298c49f06d603c1b75cb9
hurmalibademm <161161190+hurmalibademm@users.noreply.github.com>
2024-03-02 13:41:25 +0300

    Add files via upload

--- temizle.bat
+++ temizle.bat
@@ -1,6 +1,4 @@
-
 cls
-
 @echo off
 
 rem Check If User Has Admin Privileges
@@ -9,7 +7,7 @@ openfiles > NUL 2>&1
 if %errorlevel%==0 (
     echo Running..
 ) else (
-    echo HATA CALISTIRMAK ICIN YONETICI OLARAK CALISTIRIN.
+    echo HATA!! CALISTIRMAK ICIN SAG TIKLAYIP YONETICI OLARAK CALISTIRIN.
     echo.
     echo Sag tikla ve yonetici olarak calistiri'i sec.
     echo.
@@ -18,30 +16,42 @@ if %errorlevel%==0 (
     exit
 )
 
-echo.
+cls
 
 rem Delete Temporary Files
 del /s /f /q %WinDir%\Temp\*.*
+cls
 del /s /f /q %WinDir%\Prefetch\*.*
+cls
 del /s /f /q %Temp%\*.*
+cls
 del /s /f /q %AppData%\Temp\*.*
+cls
 del /s /f /q %HomePath%\AppData\LocalLow\Temp\*.*
 cls
 rem Delete Used Drivers Files (Not needed because already installed)
 del /s /f /q %SYSTEMDRIVE%\AMD\*.*
+cls
 del /s /f /q %SYSTEMDRIVE%\NVIDIA\*.*
+cls
 del /s /f /q %SYSTEMDRIVE%\INTEL\*.*
 cls
 rem Delete Temporary Folders
 rd /s /q %WinDir%\Temp
+cls
 rd /s /q %WinDir%\Prefetch
+cls
 rd /s /q %Temp%
+cls
 rd /s /q %AppData%\Temp
+cls
 rd /s /q %HomePath%\AppData\LocalLow\Temp
-clscls
+cls
 rem Delete Used Drivers Folders (Not needed because already installed)
 rd /s /q %SYSTEMDRIVE%\AMD
+cls
 rd /s /q %SYSTEMDRIVE%\NVIDIA
+cls
 rd /s /q %SYSTEMDRIVE%\INTEL
 cls
 rem Recreate Empty Temporary Folders
@@ -75,7 +85,7 @@ set /a width=80
 set /a spaces=(%width%-%deletedFolders%-36)/2
 set "space="
 for /l %%i in (1,1,%spaces%) do set "space=!space! "
-echo !space!Windows temizlendi ve ek olarak %deletedFolders% gereksiz klasor silindi.!space!
+echo !space!Bilgisayar temizlendi ve ek olarak gereksiz %deletedFolders% adet dosya basariyla silindi.!space!
 endlocal
 
 pause > NUL
